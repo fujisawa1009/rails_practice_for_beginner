efe5d70f2ed5912ed7748c97e14ada48870cd50e
YUTA FUJISAWA <mt.fuji1009@gmail.com>
2023-07-14 18:31:29 +0900

    ユーザーの詳細ページと編集ページを作る

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,28 @@ class UsersController < ApplicationController
     end
   end
 
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to question_path(params[:id]), success: 'ユーザー情報を更新しました'
+    else
+      flash.now[:danger] = '失敗しました'
+      render :edit
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile)
   end
 end
